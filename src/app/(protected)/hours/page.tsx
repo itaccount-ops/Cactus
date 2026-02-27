d7fa860e22d4cb4188524028672c722d6e2fcb24
EnriqueMEP <enrique@mep-projects.com>
2026-02-27 13:51:39 +0100

    Restore /hours redirect to /hours/daily

--- src/app/(protected)/hours/page.tsx
+++ src/app/(protected)/hours/page.tsx
@@ -1,7 +1,5 @@
 import { redirect } from 'next/navigation';
 
 export default function HoursPage() {
-    // This page has been merged into /control-horas/mi-hoja
-    // The "Registro Diario" view is the "Detalle" mode inside Mi Hoja
-    redirect('/control-horas/mi-hoja');
+    redirect('/hours/daily');
 }
